1cc04fd1b404bbaae4201eca05ee599549a1b071
AJ V <avaithyam@yahoo.com>
2025-09-25 11:44:57 -0400

    initial commit

---
+++ src/main/java/Reciept.java
@@ -0,0 +1,15 @@
+public class Reciept {
+    public static void main(String[] args) {
+        String cheese = "Cheddar cheese";
+        double cheesePrice = 5.75;
+        int cheeseQuantity = 10;
+        String milk = "2% Milk";
+        double milkPrice = 8.99;
+        int milkQuantity = 3;
+
+        System.out.println("You bought " + cheeseQuantity + " " + cheese + " for $" + cheesePrice + " each.");
+        System.out.println("You bought " + milkQuantity + " " + milk + " for $" + milkPrice + " each.");
+
+    }
+
+}
---
+++ src/main/java/VariableApp.java
@@ -0,0 +1,15 @@
+public class VariableApp {
+    public static void main(String[] args) {
+        String myFavColor = "Black";
+        int yearStarted = 2025;
+        char middleInitial = 'R';
+        boolean pets = true;
+        String niceMess = "You have a nice smile";
+
+        System.out.println("My favorite color is " + myFavColor);
+        System.out.println("I started this class in " + yearStarted);
+        System.out.println("My middle initial is " + middleInitial);
+        System.out.println("If someone asked if I have pets I would say: " + pets);
+        System.out.println("Here's a nice message for you: " + niceMess);
+    }
+}
